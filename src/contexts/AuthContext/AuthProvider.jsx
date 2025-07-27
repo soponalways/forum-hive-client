404392ba41df64082892ff701b86248492994465
soponalways <soponalways@gmail.com>
2025-07-28 02:09:14 +0600

    Hide Credential

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -36,7 +36,6 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-            console.log('user in the auth state change', currentUser)
             setLoading(false);
         });
 
