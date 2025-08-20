f561549fef822998ef1cdaa42e2ad526af93482e
soponalways <soponalways@gmail.com>
2025-08-20 18:15:29 +0600

    Profile page basic starcture create

--- src/pages/Dashboard/shared/Profile.jsx
+++ src/pages/Dashboard/shared/Profile.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import useGetRole from '../../../api/useGetRole';
+import Loading from '../../../components/Loading';
 
 const Profile = () => {
+    const { role, isLoading } = useGetRole();
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }; 
+    console.log("Loading State True on Shared Profile paege check role loading :", isLoading, role)
     return (
-        <div>
-            Hello from Profile page 
+        <div className='space-y-4 md:space-y-6 lg:space-y-8'>
+            {/* First Container  */}
+            <div className='p-3 md:p-4 lg:p-6 rounded-lg md:rounded-xl shadow shadow-primary bg-gray-500'>
+                f
+            </div>
+            {/* Second Container  */}
+            <div className='p-3 md:p-4 lg:p-6 rounded-lg md:rounded-xl shadow shadow-primary bg-gray-500'>
+                f
+            </div>
+            {/* 3rd Container  */}
+            <div className='p-3 md:p-4 lg:p-6 rounded-lg md:rounded-xl shadow shadow-primary bg-gray-500'>
+                f
+            </div>
         </div>
     );
 };
